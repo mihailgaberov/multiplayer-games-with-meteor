c0a74e00920b585875624787e8c5931a5281384b
mihailgaberov <mihail.gaberov@gmail.com>
2020-04-24 11:36:04 +0300

    :sparkles: Added GameList and update GameBoard components - ability to create new games by user and enter them.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,29 +1,50 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import Games from '../api/collections/games.js';
+import GameList from './GameList.jsx';
 import GameBoard from './GameBoard.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedGameId: null,
+    }
+  }
+
+  handleEnterGame(gameId) {
+    this.setState({ selectedGameId: gameId });
+  }
+
+  handleBackToGameList() {
+    this.setState({ selectedGameId: null });
+  }
+
+  selectedGame() {
+    return _.find(this.props.games, (game) => {
+      return game._id === this.state.selectedGameId;
+    });
+  }
+
   render() {
-    return (
-      <div>
-        {this.props.game?
-          <GameBoard game={this.props.game}/>
-          : null}
-      </div>
-    )
+    if (this.state.selectedGameId === null) {
+      return (
+        <GameList
+          games={this.props.games}
+          enterGameHandler={this.handleEnterGame.bind(this)} />
+      )
+    } else {
+      return (
+        <GameBoard
+          game={this.selectedGame()}
+          backToGameListHandler={this.handleBackToGameList.bind(this)} />
+      )
+    }
   }
 }
-/*
 
-export default createContainer(() => {
-  return {
-    game: Games.findOne()
-  };
-}, App);
-*/
 export default withTracker(() => {
   return {
-    game: Games.findOne()
+    games: Games.find().fetch()
   };
 })(App);
